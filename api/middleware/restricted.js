77d36faaf53f0afe1d2e892d2c976e9148b4c760
Addow Abdi <adowabdi007@icloud.com>
2025-05-19 05:37:25 -0500

    wip

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET || 'shh';
+
 module.exports = (req, res, next) => {
-  next();
-  /*
-    IMPLEMENT
+  const token = req.headers.authorization;
 
-    1- On valid token in the Authorization header, call next.
+  if (!token) {
+    return res.status(401).json({ message: "token required" });
+  }
 
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
-
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      return res.status(401).json({ message: "token invalid" });
+    } else {
+      req.decodedToken = decodedToken;
+      next();
+    }
+  });
 };
